0c8a7131386de51269f31ba58f7593b405a72433
sunisdown <isdown.sun@gmail.com>
2014-07-25 08:32:21 +0000

    quick union

---
+++ quick_union.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Auth Sunisdown
+
+class QuickUnion(object):
+
+    def __init__(self, N):
+        self.id_list = [i for i in xrange(N)]
+
+    def find(self, p):
+        while p != self.id_list[p]:
+            p = self.id_list[p]
+        return p
+
+    def connect(self, p, q):
+        return self.find(p) == self.find(q)
+
+    def union(self, p, q):
+        print  p, p, self.id_list
+        p_root = self.find(p)
+        q_root = self.find(q)
+        if q_root == p_root:
+            return True
+        self.id_list[p_root] = q_root
+        return False
+
+if __name__ == "__main__":
+    uf = QuickUnion(10)
+    from random import randint
+    for _ in xrange(100):
+        a = randint(0, 9)
+        b = randint(0, 9)
+        if uf.connect(a, b):
+            print True, a,b
+            continue
+        uf.union(a, b)
+        print a,b
+
