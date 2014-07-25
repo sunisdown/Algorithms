6c0ed6d2e7ba1fe37e0bc0084b545cf2d5efa378
sunisdown <isdown.sun@gmail.com>
2014-07-25 08:32:08 +0000

    quick find

--- quick_find.py
+++ quick_find.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Auth Sunisdown
+# union find 的一种方法，quick find
 
 class QuickFind(object):
-    def __init__(self):
-        self.id_list = [i for i in xrange(10)]
+    def __init__(self, id_list = []):
+        self.id_list = id_list
         self.count = 10
 
     def find(self, q):
         return self.id_list[q]
 
     def union(self, p, q):
-        print q, p, self.id_list
+        print q, p, self.id_list, self.count
         p_id = self.find(p)
         q_id = self.find(q)
 
@@ -25,7 +26,7 @@ class QuickFind(object):
         return False
 
 if __name__ == "__main__":
-    a = QuickFind()
+    a = QuickFind([i for i in xrange(10)])
     from random import randint
     for _ in xrange(10):
         print a.union(randint(0, 9), randint(0, 9))
