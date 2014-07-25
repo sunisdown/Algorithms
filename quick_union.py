b91b7b31b548b257d06fbe40efad3d21dd981b8a
sunisdown <isdown.sun@gmail.com>
2014-07-25 08:40:16 +0000

    quick union add annotation

--- quick_union.py
+++ quick_union.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Auth Sunisdown
+# union find 的一种方法，quick union
 
 class QuickUnion(object):
 
     def __init__(self, N):
         self.id_list = [i for i in xrange(N)]
+        self.count = 10
 
     def find(self, p):
         while p != self.id_list[p]:
@@ -16,12 +18,13 @@ class QuickUnion(object):
         return self.find(p) == self.find(q)
 
     def union(self, p, q):
-        print  p, p, self.id_list
+        print  p, p, self.id_list, self.count
         p_root = self.find(p)
         q_root = self.find(q)
         if q_root == p_root:
             return True
         self.id_list[p_root] = q_root
+        self.count -= 1
         return False
 
 if __name__ == "__main__":
