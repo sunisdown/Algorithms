6a1d4cdac3328f5e25c6502d9b328b53ae620f0c
SunisDown <isdown.sun@gmail.com>
2016-04-10 14:35:30 +0800

    doc: remove comment
    去掉注释调的代码

--- src/arrayqueue.go
+++ src/arrayqueue.go
@@ -12,8 +12,6 @@ type ArrayQueueA struct {
 
 func NewArrayQueueA() *ArrayQueueA {
 	return &ArrayQueueA{}
-	// q := &ArrayQueueA{}
-	// return q //直接return
 }
 
 func (lq *ArrayQueueA) size() int { // lq
@@ -27,13 +25,6 @@ func (lq *ArrayQueueA) Enqueue(v int) error {
 	lq.list[(lq.head % 5)] = v
 	lq.head = lq.head + 1
 	return nil
-	// if lq.checkEnqueue() == false { //!
-	// 	return errors.New("queue is full")
-	// } else { // 移出
-	// 	lq.list[(lq.head % 5)] = v
-	// 	lq.head = lq.head + 1
-	// 	return nil
-	// }
 }
 
 func (lq *ArrayQueueA) Dequeue() (int, error) {
@@ -43,31 +34,14 @@ func (lq *ArrayQueueA) Dequeue() (int, error) {
 	v := lq.list[(lq.tail % 5)] //
 	lq.tail = lq.tail + 1
 	return v, nil
-	// if lq.checkDequeue() == false {
-	// 	return 0, errors.New("queue is empty")
-	// } else {
-	// 	v := lq.list[(lq.tail % 5)] //
-	// 	lq.tail = lq.tail + 1
-	// 	return v, nil
-	// }
 }
 
 func (lq *ArrayQueueA) checkDequeue() bool {
 	return lq.head > lq.tail
-	// if lq.head > lq.tail { // return > bool
-	// 	return true
-	// } else {
-	// 	return false
-	// }
 }
 
 func (lq *ArrayQueueA) checkEnqueue() bool {
 	return !((lq.head%5) == (lq.tail%5) && (lq.head > lq.tail))
-	// if (lq.head%5) == (lq.tail%5) && (lq.head > lq.tail) {
-	// 	return false
-	// } else {
-	// 	return true
-	// }
 }
 
 func main() {
